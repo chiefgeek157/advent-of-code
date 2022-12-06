a7d16e9e197dd677f3beda8bfd503373bc616454
Roger Cass <rcass@unum.com>
2022-12-06 05:31:47 -0500

    2015 day22 finally!

--- 2015/day22/wiz.py
+++ 2015/day22/wiz.py
@@ -1,73 +1,104 @@
 from sys import maxsize
 
+part2 = True
+
 wiz_hp = 50
 wiz_mana = 500
 
-# boss_hp = 51
-boss_hp = 1
+boss_hp = 51
 boss_damage = 9
 
 spells = 'MDSPR'
 costs = {'M': 53, 'D': 73, 'S': 113, 'P': 173, 'R': 229 }
 
-def round(wiz_turn, wizard_hp, boss_hp, mana, spell, shield_t, poison_t,
-        recharge_t, cum_cost, cum_spells) -> int:
+def round(min_cost, wiz_turn,
+        wizard_hp, boss_hp, mana, spell,
+        shield_t, poison_t, recharge_t,
+        cum_cost, cum_spells) -> int:
     """Return additional cost to win"""
-    global spells, costs, boss_damage
+    global spells, costs, boss_damage, part2
 
-    # print(f'WT:{wiz_turn}, WHP:{wizard_hp}, BHP:{boss_hp}, M:{mana}, S:{spell}'
-    #     f', ST:{shield_t}, PT:{poison_t}, RT:{recharge_t}, CC:{cum_cost}'
-    #     f', CS:{cum_spells}')
+    cum_spells += spell if spell is not None else ''
+    # print(f'{cum_spells} MC:{min_cost} WT:{wiz_turn}, WHP:{wizard_hp}, BHP:{boss_hp}, M:{mana}, S:{spell}'
+    #     f', ST:{shield_t}, PT:{poison_t}, RT:{recharge_t}, CC:{cum_cost}')
+
+    if part2 and wiz_turn:
+        wizard_hp -= 1
+        if wizard_hp <= 0:
+            # Wizard lost
+            # print(f'{cum_spells} Lost')
+            return (maxsize, cum_spells)
+
     cost = costs[spell] if spell is not None else 0
+    if wiz_turn:
+        if cost > mana:
+            # Truncate this branch based on cost
+            # print(f'{cum_spells} Lost not enough mana')
+            return (maxsize, cum_spells)
+        if wiz_turn and cum_cost + cost > min_cost:
+            # Truncate this branch based on cost
+            # print(f'{cum_spells} Lost greater than min cost {cum_cost + cost}')
+            return (maxsize, cum_spells)
 
     # Effects apply regardless of whose turn it is
     boss_hp -= 3 if poison_t > 0 else 0
+    if boss_hp <= 0:
+        # Wizard won, candidate min cost is cum cost + cost
+        # print(f'{cum_spells} Won for {cum_cost + cost}')
+        return (min(min_cost, cum_cost + cost), cum_spells)
+
     armor = 7 if shield_t > 0 else 0
     mana += 101 if recharge_t > 0 else 0
     shield_t = max(0, shield_t - 1)
     poison_t = max(0, poison_t - 1)
     recharge_t = max(0, recharge_t - 1)
 
-    if boss_hp <= 0:
-        print(f'Won for {cum_cost + cost}')
-        return cum_cost + cost
-
     if wiz_turn:
         boss_hp -= 4 if spell == 'M' else 0
         boss_hp -= 2 if spell == 'D' else 0
+        if boss_hp <= 0:
+            # Wizard won, candidate min cost is cum cost + cost
+            # print(f'{cum_spells} Won for {cum_cost + cost}')
+            return (min(min_cost, cum_cost + cost), cum_spells)
+
         wizard_hp += 2 if spell == 'D' else 0
         shield_t += 6 if spell == 'S' and shield_t == 0 else 0
         poison_t += 6 if spell == 'P' and poison_t == 0 else 0
         recharge_t += 5 if spell == 'R' and recharge_t == 0 else 0
-        if boss_hp <= 0:
-            print(f'Won for {cum_cost + cost}')
-            return cum_cost + cost
 
-        return round(not wiz_turn, wizard_hp, boss_hp, mana - cost,
-            None, shield_t, poison_t, recharge_t,
-            cum_cost + cost, cum_spells + spell)
+        return round(min_cost, not wiz_turn,
+            wizard_hp, boss_hp, mana - cost, None,
+            shield_t, poison_t, recharge_t,
+            cum_cost + cost, cum_spells)
     else:
         wizard_hp -= max(1, boss_damage - armor)
         if wizard_hp <= 0:
-            # print(f'Lost')
-            return maxsize
+            # Wizard lost
+            # print(f'{cum_spells} Lost')
+            return (maxsize, cum_spells)
 
-        min_cost = maxsize
+        min_spells = cum_spells
         for next_spell in spells:
-            # Keep going if Wizard can afford the next spell and
-            # the cummulative cost plus the next spell is less than the
-            # minimum cost already found
-            next_spell_cost = costs[next_spell]
-            if next_spell_cost <= mana and cum_cost + next_spell_cost < min_cost:
-                min_cost = min(min_cost,
-                    round(not wiz_turn, wizard_hp, boss_hp, mana,
+            next_min_cost, next_min_spells = round(min_cost, not wiz_turn,
+                    wizard_hp, boss_hp, mana,
                     next_spell, shield_t, poison_t, recharge_t,
-                    cum_cost, cum_spells))
-        return min_cost
+                    cum_cost, cum_spells)
+            if next_min_cost < min_cost:
+                min_cost = next_min_cost
+                min_spells = next_min_spells
+                print(f'{cum_spells} NEW MIN {min_cost}')
+        return (min_cost, min_spells)
 
 min_cost = maxsize
+min_spells = ''
 for spell in spells:
-    min_cost = min(min_cost,
-        round(True, wiz_hp, boss_hp, wiz_mana, spell, 0, 0, 0, 0, ''))
+    if costs[spell] <= min_cost:
+        next_min_cost, next_min_spells = round(min_cost, True,
+            wiz_hp, boss_hp, wiz_mana, spell,
+            0, 0, 0, 0, '')
+        if next_min_cost < min_cost:
+            min_cost = next_min_cost
+            min_spells = next_min_spells
+            print(f'NEW MIN {min_cost} @ {min_spells}')
 
-print(F'Ans: {min_cost}')
+print(F'Ans: {min_cost} @ {min_spells}')
