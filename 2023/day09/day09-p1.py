c1d7c5188823c50081d88a505a5597bc8cdc0873
chiefgeek157 <rogerjcass@gmail.com>
2023-12-13 19:13:02 -0800

    2023 day09

---
+++ 2023/day09/day09-p1.py
@@ -0,0 +1,32 @@
+filename = '2023/day09/input.txt'
+# filename = '2023/day09/test1.txt'
+
+def main() -> int:
+    sum_next = 0
+    with open(filename, 'r') as f:
+        line = f.readline()
+        while line:
+            vals = list(int(x) for x in line.strip().split())
+
+            rows = [vals]
+            row = rows[0]
+            print(f'Start: {row}')
+            while any([x != 0 for x in row]):
+                next_row = []
+                rows.append(next_row)
+                for i in range(len(row) - 1):
+                    next_row.append(row[i + 1] - row[i])
+                print(f'  Next row: {next_row}')
+                row = next_row
+            for i in reversed(range(len(rows) - 1)):
+                rows[i].append(rows[i][-1] + rows[i + 1][-1])
+                print(f'  Rows[{i}] now {rows[i]}')
+            print(f'  Adding {rows[0][-1]}')
+            sum_next += rows[0][-1]
+
+            line = f.readline()
+
+    return sum_next
+
+if __name__ == '__main__':
+    print(f'Answer: {main()}')
